b6d73f6311e1368c21383cf92d3b60d09539cb53
yi-pian <yeyingxin2005@gmail.com>
2026-09-02 23:08:19 +0800

    deleteTest

--- MSPM0_Signal_Contest/10_tests/pc/build_current_debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/YiPian/Desktop/codex/yaoqingsai/MSPM0_Signal_Contest/10_tests/pc
-# Build directory: C:/Users/YiPian/Desktop/codex/yaoqingsai/MSPM0_Signal_Contest/10_tests/pc/build_current_debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(signal_library_tests "C:/Users/YiPian/Desktop/codex/yaoqingsai/MSPM0_Signal_Contest/10_tests/pc/build_current_debug/signal_library_tests.exe")
-set_tests_properties(signal_library_tests PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/YiPian/Desktop/codex/yaoqingsai/MSPM0_Signal_Contest/10_tests/pc/CMakeLists.txt;51;add_test;C:/Users/YiPian/Desktop/codex/yaoqingsai/MSPM0_Signal_Contest/10_tests/pc/CMakeLists.txt;0;")
